8efa83fe45afed958f1f43474f1200300428e223
Ramesh Sampath <ramesh@sampathweb.com>
2016-08-11 12:51:56 -0700

    initial commit

---
+++ {{cookiecutter.repo_name}}/app/settings.py
@@ -0,0 +1,82 @@
+"""
+Appliction configuration settings
+"""
+import os
+
+from tornado.options import define
+
+define("debug", default=True, help="Debug settings")
+define("port", default=9000, help="Port to run the server on")
+
+_CUR_DIR = os.path.dirname(os.path.realpath(__file__))
+MODEL_DIR = os.path.join(_CUR_DIR, "..", "models")
+LOG_DIR = os.path.join(_CUR_DIR, "..", "logs")
+
+# Auth Settings
+AUTH_KEY = "Secret Key"
+AUTH_SECRET = "You will never guess!"
+
+MAX_THREAD_POOL = 10
+
+LOG_SETTINGS = {
+    'version': 1,
+    "root": {
+        "level": "WARNING",
+        "handlers": ["console"],
+    },
+    "formatters": {
+        "json": {
+            "()": "app.utils.log_formatters.JSONFormatter",
+        },
+        "simple": {
+            "format": "%(levelname)s %(message)s",
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'formatter': "simple"
+        },
+        'applog': {
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            'level': "INFO",
+            'formatter': "json",
+            'filename': os.path.join(LOG_DIR, "app.log"),
+            "when": "D",
+            "interval": 2,
+            "backupCount": 5
+        },
+        'accesslog': {
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            'level': "INFO",
+            'formatter': "json",
+            'filename': os.path.join(LOG_DIR, "access.log"),
+            "when": "D",
+            "interval": 2,
+            "backupCount": 5
+        },
+    },
+    "loggers": {
+        "tornado.access": {
+            "handlers": ["console", "accesslog"],
+            "level": "INFO",
+            "propagate": False,
+        },
+        "tornado.application": {
+            "handlers": ["console", "applog"],
+            "level": "INFO",
+            "propagate": False,
+        },
+        "tornado.general": {
+            "handlers": ["console", "applog"],
+            "level": "INFO",
+            "propagate": False,
+        },
+        "app": {
+            "handlers": ["console", "applog"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    }
+}
